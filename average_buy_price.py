e9c9adbcd65b6a0a3a093a2c7866e258bea3165d
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2022-10-24 17:42:04 +0700

    Initial commit

---
+++ average_buy_price.py
@@ -0,0 +1,32 @@
+purchases = []
+counter = 0
+
+def main():
+    isRunning = True
+    while isRunning == True:
+        init_purchase()
+        choice = input('Do you want to add another purchase? (Y/N): ').lower()
+        while (choice == '') or (choice[0] != 'y') and (choice[0] != 'n'):
+            choice = input('Do you want to add another purchase? (Y/N): ').lower()
+        if choice[0] == 'y':
+                isRunning = True
+        if choice[0] == 'n':
+                isRunning = False
+                avg_calc()
+
+def init_purchase():
+    global counter
+    buy_price = eval(input(f'Please enter the buy price for purchase number {counter + 1}: '))
+    purchases.append(buy_price)
+    counter += 1
+    print(*purchases, sep='\n')
+    print(f'Number of purchases: {counter}')
+    
+def avg_calc():
+    result = int(sum(purchases) / counter)
+    print(f'Your average buy price is: {result}€')
+    print(f'Number of purchases: {counter}')
+
+main()
+
+
